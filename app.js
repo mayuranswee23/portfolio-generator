2ef78dc04925f2985acf756747b1d8a0310d5d24
Mayuran <mayuranswee23@gmail.com>
2021-06-16 21:01:15 -0400

    generate HTML page changes

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const fs = require('fs');
-const generatePage = require('src/page-template');
+const generatePage = require('./src/page-template.js')
 
 const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
-
 const [name, github] = profileDataArgs;
 
-console.log(name, github);
+// const generatePage = (name, github) => {
+//   return `
+//   <!DOCTYPE html> 
+//   <html lang="en"> 
+//   <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Portfolio Demo</title>
+//   </head>
 
-const pageHTML = generatePage(name, github);
+//   <body>
+//     <h1>${name}</h1>
+//     <h2><a href="https://github.com/${github}">Github</a></h2>
+//   </body>
+//   </html>
+//   `;
+// };
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
