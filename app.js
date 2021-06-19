2f0f29681a69f607bccd18adf0e21032a3a265d3
Mayuran <mayuranswee23@gmail.com>
2021-06-18 21:59:19 -0400

    final touches

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
+const {writeFile, copyFile} = require('./util/generate-site.js');
 const generatePage = require('./src/page-template.js')
 
 // const pageHTML = generatePage(name, github);
@@ -149,11 +150,19 @@ const promptProject = portfolioData => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+  
